c3c88f5598c8c77a9d2c88a58c47f29935974408
Pau Oliva <pau@eslack.org>
2013-07-11 04:23:52 +0200

    improve apk cert script

--- android/apkcert.sh
+++ android/apkcert.sh
@@ -5,6 +5,7 @@ if [ ! -e "$1" ]; then
 	exit 1
 fi
 
-unzip $1 META-INF/CERT.RSA -d /tmp/apkcert.$$/
-openssl pkcs7 -inform DER -in /tmp/apkcert.$$/META-INF/CERT.RSA -noout -print_certs -text
+unzip $1 META-INF/*.RSA -d /tmp/apkcert.$$/
+openssl pkcs7 -inform DER -in /tmp/apkcert.$$/META-INF/*.RSA -noout -print_certs -text
 #openssl pkcs7 -inform DER -in /tmp/META-INF/CERT.RSA -out CERT.pem -outform PEM -print_certs
+jarsigner -verify -certs -verbose $1
