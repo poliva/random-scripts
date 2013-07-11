a7ff6d5cd864332d2c282cd3cac7af584bb3e835
Pau Oliva <pau@eslack.org>
2013-07-11 05:12:58 +0200

    add keytool to apkcert

--- android/apkcert.sh
+++ android/apkcert.sh
@@ -8,4 +8,5 @@ fi
 unzip $1 META-INF/*.RSA -d /tmp/apkcert.$$/
 openssl pkcs7 -inform DER -in /tmp/apkcert.$$/META-INF/*.RSA -noout -print_certs -text
 #openssl pkcs7 -inform DER -in /tmp/META-INF/CERT.RSA -out CERT.pem -outform PEM -print_certs
+keytool -printcert -file /tmp/apkcert.$$/META-INF/*.RSA
 jarsigner -verify -certs -verbose $1
