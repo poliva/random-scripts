19e80e5c99d0bf7e17a855f2ef74840204982b22
Pau Oliva <pau@eslack.org>
2013-11-07 03:18:21 +0100

    use 'pf' instead of px to avoid colors, and use internal grep instead of awk. Thanks @trufae for the optimization tips :)

--- android/oat2dex.sh
+++ android/oat2dex.sh
@@ -7,14 +7,14 @@ if [ -z "${OAT}" ]; then
 	echo "Usage: $0 <file.oat>"
 	exit 0
 fi
-HIT=$(r2 -q -c '/ dex\n035' "${OAT}" 2>/dev/null |grep hit0_0 |awk '{print $1}')
+HIT=$(r2 -n -q -c '/ dex\n035' "${OAT}" 2>/dev/null |grep hit0_0 |awk '{print $1}')
 if [ -z "${HIT}" ]; then
 	echo "[-] ERROR: Can't find dex header"
 	exit 1
 else
 	echo "[+] DEX header found at address: ${HIT}"
 fi
-SIZE=$(r2 -e scr.color=false -q -c "px 4 @$HIT+32" ${OAT} 2>/dev/null |tail -n 1 |awk '{print $2 $3}' |sed -e "s/^/0x/" |rax2 -e)
+SIZE=$(r2 -n -q -c "pf i @${HIT}+32 ~[2]" "${OAT}" 2>/dev/null)
 echo "[+] Dex file size: ${SIZE} bytes"
 r2 -q -c "pr ${SIZE} @${HIT} > ${OAT}.dex" "${OAT}" 2>/dev/null
 if [ $? -eq 0 ]; then
