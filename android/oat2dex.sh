e83fee7e713d99d92b73b55f91e32778076aa7df
Anestis Bechtsoudis <anestis@census-labs.com>
2014-07-15 13:06:42 +0300

    Add multi-dex dump support

--- android/oat2dex.sh
+++ android/oat2dex.sh
@@ -7,18 +7,24 @@ if [ -z "${OAT}" ]; then
 	echo "Usage: $0 <file.oat>"
 	exit 0
 fi
-HIT=$(r2 -n -q -c '/ dex\n035' "${OAT}" 2>/dev/null |grep hit0_0 |awk '{print $1}')
-if [ -z "${HIT}" ]; then
-	echo "[-] ERROR: Can't find dex header"
+HITS=( $(r2 -n -q -c '/ dex\n035' "${OAT}" 2>/dev/null |grep hit |awk '{print $1}') )
+if [ ${#HITS[@]} -eq 0 ]; then
+	echo "[-] ERROR: Can't find dex headers"
 	exit 1
+elif [ ${#HITS[@]} -eq 1 ]; then
+	echo "[+] DEX header found at address: ${HITS[0]}"
 else
-	echo "[+] DEX header found at address: ${HIT}"
+	echo "[+] Multiple DEX headers found at addresses:"
+	for addr in ${HITS[@]}; do echo "  $addr"; done
 fi
-SIZE=$(r2 -n -q -c "pf i @${HIT}+32 ~[2]" "${OAT}" 2>/dev/null)
-echo "[+] Dex file size: ${SIZE} bytes"
-r2 -q -c "pr ${SIZE} @${HIT} > ${OAT}.dex" "${OAT}" 2>/dev/null
-if [ $? -eq 0 ]; then
-	echo "[+] Dex file dumped to: ${OAT}.dex"
-else
-	echo "[-] ERROR: Something went wrong :("
-fi
+
+for DEX_ADDR in ${HITS[@]}; do
+	SIZE=$(r2 -n -q -c "pf i @${DEX_ADDR}+32 ~[2]" "${OAT}" 2>/dev/null)
+	echo "[+] Dex file size: ${SIZE} bytes"
+	r2 -q -c "pr ${SIZE} @${DEX_ADDR} > ${OAT}.${DEX_ADDR}.dex" "${OAT}" 2>/dev/null
+	if [ $? -eq 0 ]; then
+		echo "[+] Dex file dumped to: ${OAT}.${DEX_ADDR}.dex"
+	else
+		echo "[-] ERROR: Something went wrong :("
+	fi
+done
